3273e8bfce7d87b3a6b33e6484eb065b48060139
Daniel Falkner <dafalkne@microsoft.com>
2020-05-15 14:09:19 +0200

    Added Vnet

--- demo.tf
+++ demo.tf
@@ -15,3 +15,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_prefix}TFResourceGroup"
   location = var.location
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_prefix}TFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
