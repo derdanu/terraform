8ec05e54039f0f62f24a9bd159a20ef32c15df63
Daniel Falkner <dafalkne@microsoft.com>
2020-05-15 14:19:06 +0200

    nsg

--- demo.tf
+++ demo.tf
@@ -37,6 +37,24 @@ resource "azurerm_public_ip" "publicip" {
   allocation_method   = "Static"
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.resource_prefix}TFNSG"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 output "instance_ip_addr" {
   value       = azurerm_public_ip.publicip.ip_address
   description = "The public IP address of the instance."
