e24aba72e830a501ed1551d0b81b3864d4465986
Daniel Falkner <dafalkne@microsoft.com>
2020-05-15 14:13:46 +0200

    add pip

--- demo.tf
+++ demo.tf
@@ -29,3 +29,16 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "10.0.1.0/24"
 }
+
+resource "azurerm_public_ip" "publicip" {
+  name                = "${var.resource_prefix}TFPublicIP"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+
+data "azurerm_public_ip" "publicip" {
+  name                = azurerm_public_ip.publicip.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
