eaef274fba0a188726026b45d957b52af130e3e0
Daniel Falkner <dafalkne@microsoft.com>
2020-05-15 14:12:51 +0200

    add subnet

--- demo.tf
+++ demo.tf
@@ -22,3 +22,10 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.resource_prefix}TFSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.1.0/24"
+}
