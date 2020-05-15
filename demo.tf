7b04f725940f28bfe50ecfb48ccb74996d8f0c86
Daniel Falkner <dafalkne@microsoft.com>
2020-05-15 15:00:23 +0200

    typo

--- demo.tf
+++ demo.tf
@@ -24,7 +24,7 @@ variable "location" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_prefix}${var.variable.resource_group_name}"
+  name     = "${var.resource_prefix}${var.resource_group_name}"
   location = var.location
 }
 
