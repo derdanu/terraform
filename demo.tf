fcabad5a01c98d4e039947c28ee36f60c4dc2a63
Daniel Falkner <dafalkne@microsoft.com>
2020-05-15 14:35:09 +0200

    nsg

--- demo.tf
+++ demo.tf
@@ -59,7 +59,6 @@ resource "azurerm_network_interface" "nic" {
   name                      = "${var.resource_prefix}NIC"
   location                  = var.location
   resource_group_name       = azurerm_resource_group.rg.name
-  network_security_group_id = azurerm_network_security_group.nsg.id
 
   ip_configuration {
     name                          = "${var.resource_prefix}NICConfg"
@@ -67,6 +66,44 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "nicnsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "${var.resource_prefix}TFVM"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "${var.resource_prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.resource_prefix}TFVM"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
 }
 
 output "instance_ip_addr" {
